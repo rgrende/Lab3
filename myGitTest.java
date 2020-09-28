e96607e1e846e55e33acb44d510349ab9d4681be
Rakiah Grende <rgrende@carroll.edu>
2020-09-28 14:26:20 -0600

    Added main class in java file

---
+++ myGitTest.java
@@ -0,0 +1,14 @@
+package gitTest;
+
+public class myGitTest {
+
+	public myGitTest() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Rakiah says Hello Git Test");/// TODO Auto-generated method stub
+
+	}
+
+}
