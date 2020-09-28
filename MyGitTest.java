b423ec72d49746776bca4c89cdc0c78c218562c1
Anna Jarman <ajarman@carroll.edu>
2020-09-28 14:33:02 -0600

    changed message in branch A

--- MyGitTest.java
+++ MyGitTest.java
@@ -3,7 +3,7 @@ package gitTest;
 public class MyGitTest {
 
 	public static void main(String[] args) {
-		System.out.println("Anna says Hello Git Test");
+		System.out.println("Anna says hello from Branch AAAA");
 
 	}
 
